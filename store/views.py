4ce0a2a46f87051f4ddb2ae608fe2164aff2f51a
RaduRu <radu.ursu@stud.unifi.it>
2024-06-17 13:26:44 +0200

    checkout page updated

--- store/views.py
+++ store/views.py
@@ -20,8 +20,15 @@ def cart(request):
 
 
 def checkout(request):
-    
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        #fixed not login cart preoblem
+        order = {'get_cart_tot': 0, 'get_cart_items':0}
+    context = {'items' : items, 'order': order}
     return render (request, 'store/checkout.html', context)
 
 def category(request):
