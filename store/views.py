1e72980cdc7de1967b1536b0aba68b3d1a00209e
RaduRu <radu.ursu@stud.unifi.it>
2024-06-18 10:53:58 +0200

    Created cookie for cart, add cookieItem, loop in cart created and created the try block

--- store/views.py
+++ store/views.py
@@ -28,10 +28,46 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        #i used this in order to prevent the error that it gaves if the coookie cart is not already created 
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {} 
+        print('Cart:', cart)
         items = []
         #fixed not login cart preoblem
         order = {'get_cart_tot': 0, 'get_cart_items':0, 'shipping':False}
         cartItems = order['get_cart_items']
+
+        for i in cart :
+            #i use i try block to prevent items in cart that may have been removed from causing an error
+            try:
+
+                cartItems += cart[i]["quantity"]
+
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]["quantity"])
+
+                order['get_cart_tot'] += total
+                order['get_cart_items'] += cart[i]["quantity"]
+
+                item = {
+                    'product': {
+                        'id': product.id,
+                        'name' : product.name,
+                        'price' : product.price,
+                        'imageURL' : product.imageURL,
+                        },
+                    'quantity': cart[i]['quantity'],
+                    'get_total': total,
+                    }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass
+
 
     context = {'items' : items, 'order': order, 'cartItems': cartItems}
     return render (request, 'store/cart.html', context)
