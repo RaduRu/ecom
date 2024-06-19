6ddda8ef44e57e11e5986aa1b7be937648a5b4f1
RaduRu <radu.ursu@stud.unifi.it>
2024-06-19 14:26:04 +0200

    part 3

--- ecom/settings.py
+++ ecom/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ytxv&m#qkx_z6@2hcw2i^f&av4qz&=(f1%cj$!o)lyiyo@xoml"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
